2b566f7c2195adf943a99b21eef555981c96b59e
github gutch910 <hisa910@hotmail.com>
2023-03-21 16:29:59 +0900

    commit all rust_resson

---
+++ generic/src/main.rs
@@ -0,0 +1,23 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    //fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, U> {        
+        Point {
+            x: self.x,
+            y: self.y,
+        }
+    }
+}
+
+fn main() {
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c'};
+
+    //let p3 = p1.mixup(p2);
+    let p3 = p2.mixup(p1);
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
---
+++ rust_lesson/src/main.rs
@@ -0,0 +1,48 @@
+// * 演習 FizzBuzz問題
+// ・１から順に数え上げ、以下の条件で出力を行う
+// ・３の倍数　    →　「Fizz」を出力
+// ・５の倍数　    →　「Buzz」を出力
+// ・３・５の倍数  →　「FizzBuzz」を出力
+// ・そのほか      →　その数値を出力
+// 
+// 仕様
+// ・fizzbuzz関数を作成し、main関数から呼び出す形で実装する
+// ・fizzbuzz関数の引数に数え上げの終端の値を渡せるようにする
+// 
+// 手法
+// ・while と if を使用した実装
+// ・for と match を使用した実装
+// ・match のパターンをタプルに置き換え
+
+fn main() {
+    //println!("Hello, world!");
+    fizzbuzz(1);
+    //let a = print(10);
+    //println!("Hello test {}", a);
+}
+
+fn fizzbuzz(x: i32) -> i32 {
+
+    let mut limit = 10;
+    //println!("{}", x);
+    
+
+    while limit != 0 {
+
+    println!("{}",limit);
+
+
+    
+
+
+     limit -= 1;   
+    }
+    limit
+
+}
+
+//fn print(x: i32) -> i32 {
+//    println!("fn test");
+    //let x = 10;
+//   x
+//}
