2a5d882d3ead2105be64dd20aa40c9969a75ff55
github gutch910 <hisa910@hotmail.com>
2023-03-26 18:53:02 +0900

    2023/03/26 RUST lesson Add&Update

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,40 +1,31 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+//use std::fs::File;
+//use std::io::prelude::*;
+use std::process;
+//use std::error::Error;
+
+use minigrep::Config;
+use minigrep::run;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    //let config = parse_config(&args);
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    // --snip--
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    // ファイルが見つかりませんでした
-    let mut f = File::open(config.filename).expect("file not found");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        // ファイルの読み込み中に問題がありました
-        .expect("something went wrong reading the file");
-
-    // テキストは\n{}です
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Config { query, filename }
+        process::exit(1);
     }
+
 }
