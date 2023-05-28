a4e277cd9d3e18a11945bb599e3a5bf30ec4531c
gutch910 <113252628+gutch910@users.noreply.github.com>
2023-05-28 15:14:59 +0900

    Delete Mgrep directory

--- Mgrep/target/debug/deps/Mgrep-3c9603dbbcddc835.d
+++
@@ -1,5 +0,0 @@
-c:\Users\hisa9\RUST_git\Mgrep\target\debug\deps\Mgrep-3c9603dbbcddc835.rmeta: src\lib.rs
-
-c:\Users\hisa9\RUST_git\Mgrep\target\debug\deps\Mgrep-3c9603dbbcddc835.d: src\lib.rs
-
-src\lib.rs:
--- Mgrep/target/debug/deps/Mgrep-466cacdfc33aec1e.d
+++
@@ -1,5 +0,0 @@
-c:\Users\hisa9\RUST_git\Mgrep\target\debug\deps\Mgrep-466cacdfc33aec1e.rmeta: src\main.rs
-
-c:\Users\hisa9\RUST_git\Mgrep\target\debug\deps\Mgrep-466cacdfc33aec1e.d: src\main.rs
-
-src\main.rs:
